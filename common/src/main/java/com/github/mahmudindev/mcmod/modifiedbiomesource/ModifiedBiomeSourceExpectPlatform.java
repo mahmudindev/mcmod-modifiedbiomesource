9ec0d11d1d84e8ff6ea1dc05c8385d26f4d5ddf3
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-08-06 04:40:43 +0700

    Initial commit

---
+++ common/src/main/java/com/github/mahmudindev/mcmod/modifiedbiomesource/ModifiedBiomeSourceExpectPlatform.java
@@ -0,0 +1,19 @@
+package com.github.mahmudindev.mcmod.modifiedbiomesource;
+
+import dev.architectury.injectables.annotations.ExpectPlatform;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.function.Supplier;
+
+public class ModifiedBiomeSourceExpectPlatform {
+    @ExpectPlatform
+    public static <T, V extends T> Supplier<V> registerRegistryEntry(
+            ResourceKey<? extends Registry<T>> resourceKey,
+            ResourceLocation resourceLocation,
+            Supplier<? extends V> supplier
+    ) {
+        return () -> null;
+    }
+}
